c767dc7fd18c897fb1bdd528ab40bfcc37b6e65f
Emmanuel Azinta <80649064+emmanuelazinta@users.noreply.github.com>
2024-03-27 13:01:08 +0100

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,25 @@
+
+
+var FN;
+
+var input = document.getElementById("input");
+var minus = document.getElementById("minus").value;
+
+
+
+function one(x){
+    var num1 = x.innerText;
+   input.value += num1;
+}
+function cleartxt(){
+    input.value = " ";
+}
+function minuss(){
+     input.value - x;
+
+}
+function pluss(){
+     input + num1 ;
+
+
+}
